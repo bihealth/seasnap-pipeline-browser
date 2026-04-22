2fdd68d8b1702f737c6489fa43ce89a818d13fc0
January Weiner <january.weiner@gmail.com>
2026-04-22 09:13:24 +0200

    updated to image 0.6.8 with new seapiper and bioshmods

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 IMAGE="${IMAGE:-ghcr.io/bihealth/seapiper}"
-TAG="${TAG:-0.6.7}"
+TAG="${TAG:-0.6.8}"
 BIOC_PKGS="${BIOC_PKGS:-DESeq2,ComplexHeatmap}"
 # tmod currently tracks master upstream.
 TMOD_REF="${TMOD_REF:-d583238f3559482b02cf34b9304a19b090006aca}"
 RSEASNAP_REF="${RSEASNAP_REF:-66f4f705dbee586a5de197322a401652c43d0598}"
-BIOSHMODS_REF="${BIOSHMODS_REF:-a35dd7a4f42a6460c9ab1a94c9e8faa41f3eaacb}"
-SEAPIPER_REF="${SEAPIPER_REF:-02cbcd9e2fd85f3728a9154ea9ece51a35453ccf}"
+BIOSHMODS_REF="${BIOSHMODS_REF:-v0.1.6}"
+SEAPIPER_REF="${SEAPIPER_REF:-v0.1.8}"
 
 docker buildx build \
   --load \
